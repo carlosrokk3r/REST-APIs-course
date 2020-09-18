c33fcc9f7bdc235601e834940f1793bd800b5c2b
Carlos Jimenez <carlos.jimenez@rokk3rlabs.com>
2020-09-18 15:07:20 -0500

    New updates for section 11

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',
                                                        'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE?EXCEPTIONS'] = True
 app.secret_key = 'carlos'
 api = Api(app)
 
